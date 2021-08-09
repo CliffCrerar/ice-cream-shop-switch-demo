edeea5bde185740d7618167b9450a498fb499da5
CliffCrerar <cliff.crerar@gmail.com>
2021-08-09 15:01:35 +0200

    2 files modified

--- startup.js
+++ startup.js
@@ -2,10 +2,29 @@
 
 const cdn = 'https://cdn.softwarecraft.co.za/ice-cream-shop/';
 (function() {
-  document.body.classList.add('container');
-  Array.from(document.body.children).forEach(child =>
-    child.classList.add('container')
+  // document.body.classList.add('container');
+  Array.from(document.body.children).forEach(child => {
+    console.log(child);
+    child.classList.add('container');
+  });
+
+  const footer = document.createElement('footer');
+  ['fixed-bottom', 'shadow', 'py-3'].forEach(className =>
+    footer.classList.add(className)
   );
+  footer.innerHTML = `
+    <div class="text-center">
+      <a target="_blank"
+        href="https://github.com/CliffCrerar/ice-cream-shop-switch-demo"><span class="bi bi-github"></span> View on
+        github </a><br>
+      <a target="_blank"
+        href="https://github.com/CliffCrerar/ice-cream-shop-switch-demo"><span  class="bi bi-lightning"></span> View on
+        Stackblitz</a><br>
+      <a target="_blank" href="https://cliffcrerar.tech"><small>About the Author</a>
+    </small>
+    </div>
+  `;
+  document.body.appendChild(footer);
   return;
 })();
 
